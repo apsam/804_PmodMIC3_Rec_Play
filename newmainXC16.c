1aa433f2786dd6023bfb61975b034832a25356ea
psam <sam445544@gmail.com>
2017-05-13 18:07:00 -0700

    Fixed MISO/MOSI pinout for MIC and FLASH

--- newmainXC16.c
+++ newmainXC16.c
@@ -49,7 +49,7 @@ void delay(int limit1, int limit2){
 /*
  *	ISR For Right Channel DAC
  *		TODO: When does this interrupt activate?
- *		W
+ *		
  */
 void __attribute__((interrupt, no_auto_psv))_DAC1RInterrupt(void){
 	IFS4bits.DAC1RIF = 0;		//Clear Right Channel Interrupt Flag
@@ -57,8 +57,8 @@ void __attribute__((interrupt, no_auto_psv))_DAC1RInterrupt(void){
 	//Only intake data when not recording	
 	if(isRecording == 0){	//Only activate when not recording	
 		if(isPlaybackStart == 1){
-			LATAbits.LATA2 = 1;		//RA2	p30
-			LATAbits.LATA1 = 1;		//GREEN
+			//LATAbits.LATA2 = 1;		//RA2	p30
+			//LATAbits.LATA1 = 1;		//GREEN
 			//Entering the playback stage
 			isPlayback = 1;
 		}
@@ -68,21 +68,13 @@ void __attribute__((interrupt, no_auto_psv))_DAC1RInterrupt(void){
 		if(counter >= SAMPLE_TIME - 10){	//Only when all of the array has been pushed out
 			replayCount++;
 			counter = 0;
-			LATAbits.LATA1 = 0;		//GREEN			
-			LATAbits.LATA2 = 0;		//RA2	p30
+			//LATAbits.LATA1 = 0;		//GREEN			
+			//LATAbits.LATA2 = 0;		//RA2	p30
 			//Handle control logic outside of ISR
 			isPlaybackStart = 0;
 			isPlayback = 0;
 		}	
 	}	
-}
-
-unsigned int SPI_Receive(){
-    while(SPI1STATbits.SPITBF == 1);
-    SPI1BUF = 0x0000;
-    while(SPI1STATbits.SPIRBF == 0);
-    
-    return SPI1BUF;
 }
 
 void clockSetup(){
@@ -101,13 +93,10 @@ void clockSetup(){
     CLKDIVbits.DOZE     = 0;    //FCY = FP/1 = 9.21MHz
     CLKDIVbits.DOZEN    = 1;    //DOZE is the ratio between CPU and periph clk
     
-    
     //ACLKCON Register = Auxiliary clock control for DAC
     ACLKCONbits.AOSCMD	 = 0;    //Aux clock is disabled
 	ACLKCONbits.SELACLK  = 0;    //Select PLL Output
     ACLKCONbits.APSTSCLR = 7;    //FA = FOSC/1 = 18.42 MHz
-	//Causes infinite loop when play back??
-	//ACLKCONbits.APSTSCLR = 0;    //FA = FOSC/1 = 18.42 MHz
 }
 
 void spiSetup(){
@@ -131,8 +120,8 @@ void spiSetup(){
     SPI1CON1bits.PPRE   = 0;    //Primary SPI clock = 9.21/64 = 143.9KHz
 	//Secondary prescale bits	3:1
     //SPI1CON1bits.SPRE   = 5;    //Second SPI clock  = 143.9/3 = 48KHz     
-	SPI1CON1bits.SPRE   = 7;    //Second SPI clock0
-	//SCK = 48KHz
+	SPI1CON1bits.SPRE   = 7;    //Second SPI clock
+	//SCK = XXKHz
     
     SPI1STATbits.SPIROV = 0;    //Clear initial overflow bit 
     SPI1STATbits.SPIEN  = 1;    //SPI1 module is enabled, SCK1, SDO1 and SS1' are PORT pins
@@ -140,24 +129,29 @@ void spiSetup(){
 
 void pinSelect(){
     PPSUnLock;
-    RPOR4bits.RP8R      = 8;    //RP8 output for SCK1
-    RPINR20bits.SCK1R   = 8;    //RP8  input for SCK1
-    RPINR20bits.SDI1R   = 9;    //RP9  input for SDI1
-    //No SDO is needed, MIC is not expecting input...
-    RPOR3bits.RP6R      = 0;    //RP6 output for SS1
-								//No peripheral mapped to RP6
-								//RP6 output is controlled by LATB
-    RPOR6bits.RP12R     = 0;    //RP12 analog output for DAC1 (DAC1RP)
-	RPOR6bits.RP13R     = 0;    //RP13 analog output for DAC1 (DAC1RN)
+	    
+	PPSUnLock;
+    RPOR4bits.RP8R = 8;     //RP8 is an output	for SCK1
+    RPINR20bits.SCK1R = 8;  //RP8 is an input	for SCK1
+    
+	RPOR4bits.RP9R = 7;     //RP9 is an output	for SDO1	
+    RPINR20bits.SDI1R = 7;  //RP7 is an input	for SDI1
+	
+	RPOR2bits.RP5R	= 9;	//RP5 output for SS1	for FLASH    
+	RPOR3bits.RP6R	= 0;    //RP6 output for SS2	for MIC
 
-    PPSLock;
+	RPOR6bits.RP12R	= 0;    //RP12 analog output for DAC1 (DAC1RP)
+	RPOR6bits.RP13R	= 0;    //RP13 analog output for DAC1 (DAC1RN)
+	PPSLock;
 }
 
 void ioSetup(){
-	//Microphone pins
-    TRISBbits.TRISB6    = 0;    //RB6 output for SS                 p42
+	//SPI Pins
     TRISBbits.TRISB8    = 0;    //RB8 output for SCK1               p44    
-	TRISBbits.TRISB9    = 1;    //RB9 input for MISO                p01
+	TRISBbits.TRISB9    = 1;    //RB9 output for MOSI               p01
+	TRISBbits.TRISB7	= 1;	//RB7 input  for MISO    
+	TRISBbits.TRISB5	= 0;	//RB5 output for SS1
+	TRISBbits.TRISB6    = 0;    //RB6 output for SS2                p42
 	
 	//DAC Pin
 	TRISBbits.TRISB12   = 0;    //RB12 output for DAC1RP            p10
@@ -165,15 +159,19 @@ void ioSetup(){
 
     //LED Debugging
     AD1PCFGL = 0xFFFF;          //AN0-AN8 are now GPIO
-    //Debugging recording/playback states
-	TRISAbits.TRISA0 = 0;       //RA0 = output      p19
-    TRISAbits.TRISA1 = 0;       //RA1 = output      p20    
-	//Debugging misc.
-    TRISAbits.TRISA8 = 0;       //RA8 = output      p32    
-	TRISAbits.TRISA4 = 0;       //RA4 = output      p34
-	TRISAbits.TRISA9 = 0;       //RA9 = output      p35		
-	TRISAbits.TRISA2 = 0;       //RA2 = output      p30
-	TRISAbits.TRISA3 = 0;       //RA3 = output      p31	
+	
+	//Button inputs
+    TRISAbits.TRISA0=1; //Pin RA0 is assigned as input for record
+    TRISAbits.TRISA1=1; //Pin RA1 is assigned as input for play
+    TRISAbits.TRISA4=1; //Pin RA4 is assigned as input for erase
+	
+	//LED Outputs 
+	TRISAbits.TRISA2=0; //Pin RA2 is assigned as output for Red LED
+    TRISAbits.TRISA3=0; //Pin RA3 is assigned as output for Green LED
+	TRISAbits.TRISA8=0; //Pin RA8 is assigned as output for Yellow LED
+	
+	//Debug
+	TRISAbits.TRISA7 = 0; //Output debug led for record
 }
 
 void setupDAC(){
@@ -212,6 +210,165 @@ void setupDAC(){
 }
 
 /*
+unsigned int SPI_Receive(){
+    while(SPI1STATbits.SPITBF == 1);
+    SPI1BUF = 0x0000;
+    while(SPI1STATbits.SPIRBF == 0);
+    
+    return SPI1BUF;
+}
+ * */
+
+unsigned char SPI_Transmit(unsigned char TxValue)
+{
+    while(SPI1STATbits.SPITBF==1);//Wait until the TX buffer is empty due to a prior process
+    SPI1BUF = TxValue;            //When empty, send the byte to the TX buffer
+    while(SPI1STATbits.SPIRBF==0);//As valid bits shifts out of SDO, junk bits are received from SDI
+                                  //Wait until the RX buffer is full of junk data
+    return SPI1BUF;               //When full, read the junk data in RX buffer through SPI1BUF
+}
+
+unsigned char SPI_Receive()
+{
+    while(SPI1STATbits.SPITBF==1);//Wait until the TX buffer is empty due to a prior process
+    SPI1BUF = 0xBB;               //When empty, send the junk byte (0x00) to the TX buffer
+    while(SPI1STATbits.SPIRBF==0);//As junk bits shifts out of SDO, valid bits are received from the SDI
+                                  //Wait until the RX buffer is full of valid data
+    return SPI1BUF;               //When full, read the valid data in RX buffer through SPI1BUF
+}
+
+void WREN()
+{
+    LATBbits.LATB5 = 0; //Lower SS for instruction delivery
+    SPI_Transmit(0x06); //Send the WREN instruction
+    LATBbits.LATB5 = 1; //Raise SS for instruction completion
+}
+
+unsigned char Read_Status_Reg()
+{
+    LATBbits.LATB5 = 0;                  //Lower SS for instruction delivery
+    SPI_Transmit(0x05);                  //Send the Read status register instruction
+    unsigned char status = SPI_Receive();//Read the status register
+    LATBbits.LATB5 = 1;                  //Raise SS for instruction completion
+    return status;
+}
+
+unsigned char Write_Byte (unsigned char command, long int address, unsigned char SentByte)
+{
+    LATBbits.LATB5 = 0;                  //Lower SS to start PP
+    SPI_Transmit(command);               //Send the PP instruction
+    SPI_Transmit((address >> 16) & 0xFF);//Send the  MS byte of the 24-bit address
+    SPI_Transmit((address >> 8) & 0xFF); //Send the mid byte of the 24-bit address
+    SPI_Transmit(address & 0xFF);        //Send the LS byte of the 24-bit address
+    SPI_Transmit(SentByte);
+    unsigned char JunkByte = SPI_Receive();
+    LATBbits.LATB5 = 1;                  //Raise SS for instruction completion
+    return JunkByte;
+}
+
+unsigned char Read_Byte (unsigned char command, long int address)
+{
+    LATBbits.LATB5 = 0;                  //Lower SS to start PP
+    SPI_Transmit(command);               //Send the PP instruction
+    SPI_Transmit((address >> 16) & 0xFF);//Send the MS byte of the 24-bit address
+    SPI_Transmit((address >> 8) & 0xFF); //Send the mid byte of the 24-bit address
+    SPI_Transmit(address & 0xFF);        //Send the LS byte of the 24-bit address
+    unsigned char ReceivedByte = SPI_Receive();
+    LATBbits.LATB5 = 1;                  //Raise SS for instruction completion
+    return ReceivedByte;
+}
+
+void clear_memory()
+{
+            //Process to clear FLASH memory
+            LATAbits.LATA8=1;   //Yellow LED on
+            LATBbits.LATB5=0;  //Lower SS for instruction delivery
+            SPI_Transmit(0x50);//Enable-Write_Status-Register opcode
+            LATBbits.LATB5=1;  //Raise SS for instruction completion
+        
+            LATBbits.LATB5=0;  //Lower SS for instruction delivery
+            SPI_Transmit(0x01);//Write-Status-Register opcode
+            SPI_Transmit(0x00);//Status Register:0x00
+                               //Must set BP2,BP1,BP0 to 0 in order to take off write protect
+            LATBbits.LATB5=1;
+    
+            LATBbits.LATB5=0;
+            SPI_Transmit(0x05);//Read status register Opcode
+            SPI_Receive();     //Read status register
+            LATBbits.LATB5=1;
+    
+
+            WREN();
+            LATBbits.LATB5=0;
+            SPI_Transmit(0xC7);//Opcode for chip erase
+            LATBbits.LATB5=1;
+    
+            while((Read_Status_Reg() & 0x01) == 0x01);//Wait for memory to be clear
+    
+            Read_Byte(0x03,0x020000);//Check when data is 0xFF
+    
+            Read_Status_Reg();            
+            delay(100,1000);
+            LATAbits.LATA8=0;   //Yellow LED off
+            LATAbits.LATA2=1;   //Red LED on
+            LATAbits.LATA3=1;   //Green LED on
+            delay(100,100);
+}
+void setup()
+{
+            WREN();
+            while((Read_Status_Reg() & 0x02)==0x00);//While WEL=0 keep reading the status register Read_Status_Reg();
+            LATBbits.LATB5=0;
+            SPI_Transmit(0xAD);//AAI Word Program instruction opcode
+            SPI_Transmit(0x02);//Address 0x020000
+            SPI_Transmit(0x00);
+            SPI_Transmit(0x00);
+            SPI_Transmit(0xAB);//Word Byte Data: 0xABCD//This will be junk
+            SPI_Transmit(0xCD);
+            LATBbits.LATB5=1;
+            while((Read_Status_Reg()& 0x01) == 0x01);//Polling Busy bit in the status register
+            
+            LATAbits.LATA8=1;   //Yellow LED on
+            LATAbits.LATA2=0;   //Red LED off
+            LATAbits.LATA3=0;   //Green LED off
+            delay(100,1000);
+}
+void record()
+{
+            LATAbits.LATA2=1;   //Red LED on
+            while((Read_Status_Reg()& 0x01) == 0x01);//Polling Busy bit in the status register
+    
+            LATBbits.LATB5=0;
+            SPI_Transmit(0xAD);//AAI opcode
+            SPI_Transmit(0xEF);//Word Byte Data: 0xEFBA Actual Data
+            SPI_Transmit(0xBA);
+            LATBbits.LATB5=1;
+}
+void exit_record()
+{
+            LATAbits.LATA3=1;   //Green LED on
+            
+            LATBbits.LATB5=0;    
+            SPI_Transmit(0x04);//Opcode WRDI to exit AAI Mode
+            LATBbits.LATB5=1;
+}
+void play()//read data
+{
+            Read_Status_Reg();//Read status register
+            LATBbits.LATB5=0;
+            SPI_Transmit(0x03);//Read opcode
+            SPI_Transmit(0x02);//3-byte start address
+            SPI_Transmit(0x00);
+            SPI_Transmit(0x00);
+            while(SPI_Receive()!=0xFF)//Read all the data that was written until we reach the area of memory that is clear
+            {
+                SPI_Receive();
+            }
+            LATBbits.LATB5=1;
+}
+
+
+/*
  *	Interpreting the digital data to voltages...
  *	When FORM = 0 (unsigned integer):
  *		0xFFFF		Most positive output voltage
@@ -232,21 +389,14 @@ int main(void) {
     setupDAC();
     
 	//Debug LEDS
-    LATAbits.LATA0 = 0;		//RA0	p19
-    LATAbits.LATA1 = 0;		//RA1	p20
-	
-    LATAbits.LATA8 = 0;		//RA8	p32
-    LATAbits.LATA4 = 0;		//RA4	p34
-    LATAbits.LATA9 = 0;		//RA9	p35
-    LATAbits.LATA2 = 0;		//RA2	p30
-    LATAbits.LATA3 = 0;		//RA3	p31
+    LATAbits.LATA7 = 0;
 	
     i = 0;
     while(1){
 		//Record for some predetermined time length
 		if(isRecording){
-			//Turn ON RED LED (p19)= Recording
-			LATAbits.LATA0 = 1;
+			//Recording
+			LATAbits.LATA7 = 1;
 			//What does this delay do?
 			delay(400, 400);
 			// Record for some length of time, feed contents into global array
@@ -261,8 +411,8 @@ int main(void) {
 			}
 			//No longer recording
 			isRecording = 0;
-			//Turn OFF RED LED (p19)= Not Recording
-			LATAbits.LATA0 = 0;	
+			//Not Recording
+			LATAbits.LATA7 = 0;	
 			//Start the playback
 			isPlaybackStart = 1;
 			
@@ -272,25 +422,6 @@ int main(void) {
 		}
 		//USE INTERRUPTS INSTEAD OF STATUS REGISTER*********************
 		//Toggle debug LED to visualize steps...
-        switch(replayCount){
-			case 0:			
-				LATAbits.LATA8 = 1;		//RA8	p32
-				delay(100,1000);
-				LATAbits.LATA8 = 0;		//RA8	p32
-				break;
-			case 1:
-				LATAbits.LATA4 = 1;		//RA4	p34
-				delay(100,1000);
-				LATAbits.LATA4 = 0;		//RA4	p34
-				break;
-			case 2:				
-				LATAbits.LATA9 = 1;		//RA9	p35
-				delay(100,1000);
-				LATAbits.LATA9 = 0;		//RA9	p35
-				break;
-			default:
-				break;
-		}
 		
 		if(replayCount >= replayLimit){
 			//Restart the process
